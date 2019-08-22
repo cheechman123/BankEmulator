80806839cbcc3e4aa8e159c6125738dfdd6b349e
cheechman123 <cheechman123@gmail.com>
2019-08-22 12:55:35 +0300

    Refactor old code,add new funcional

--- src/whyNotPractice/Bank.java
+++ src/whyNotPractice/Bank.java
@@ -1,16 +1,22 @@
 package whyNotPractice;
 
+import java.io.IOException;
+
 public class Bank {
-    private double persent;
-    private int month;
+    private final double persent;
+    private final int month;
+    User user ;
 
     public Bank(double persent, int month){
         this.persent = persent;
         this.month = month;
     }
 
-    public static double persentCount(double startCapital, double persent, int additionalAmount, double month) {
+    public  double persentCount(double startCapital, double persent, int additionalAmount, double month) {
         double resOfSub = 0;
+        if(this.persent != persent && this.month != month) {
+            throw new IllegalArgumentException("Sorry this service not supported ");
+        }
         for (int i = 0; i < month - 1; i++) {
             double persentCalculating = (startCapital * persent) / month;
             resOfSub += persentCalculating;
