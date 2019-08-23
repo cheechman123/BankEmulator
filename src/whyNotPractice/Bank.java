51d796973b31a78332d115c3fa2b130e4582d8dc
cheechman123 <cheechman123@gmail.com>
2019-08-23 11:02:57 +0300

    Add javadoc to "Bank" class

--- src/whyNotPractice/Bank.java
+++ src/whyNotPractice/Bank.java
@@ -1,21 +1,32 @@
 package whyNotPractice;
 
 import java.io.IOException;
-
+/**
+ * Class Bank with property <b>persent</b> and <b>month</b>
+ * @autor FaiFai
+ * @version 1.0
+ */
 public class Bank {
+    /** Field persent*/
     private final double persent;
+    /** Field month*/
     private final int month;
 
-
+    /**
+     *Constructor - creating new object
+     * @see Bank#Bank(double, int)
+     * @param persent
+     * @param month
+     */
     public Bank(double persent, int month) {
         this.persent = persent;
         this.month = month;
     }
-
+    /** Function receipt field value {@link Bank#persent}*/
     public double getPersent() {
         return persent;
     }
-
+    /** Function receipt field value {@link Bank#month}*/
     public int getMonth() {
         return month;
     }
