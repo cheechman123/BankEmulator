20d14bc0f3ef4dbfa7ba39d569e35b38038767df
cheechman123 <cheechman123@gmail.com>
2019-08-23 11:13:27 +0300

    Add javadoc to "PersentCalculator" class

--- src/whyNotPractice/PercentCalculator.java
+++ src/whyNotPractice/PercentCalculator.java
@@ -1,9 +1,20 @@
 package whyNotPractice;
-
+/**
+ * Class PercentCalculator with property <b>user</b> and <b>bank</b>
+ * @autor FaiFai
+ * @version 1.0
+ */
 public class PercentCalculator {
+    /** Object of class User - user*/
     User user;
+    /** Object of class Bank - bank*/
     Bank bank;
 
+    /**
+     * Method calculate user's deposit , and check what tariff user want with tariff witch bank can offed
+     * @param user
+     * @return
+     */
     public  double calculate( User user) {
         double resOfSub = 0;
         if(user.getPersent() != bank.getPersent() && user.getNumOfMonth() != bank.getMonth()) {
